4b6c88bb327032dbbb0f9935396368361afb5724
mvnh <dider90n2@gmail.com>
2024-10-01 19:30:55 +0300

    move suspendTransaction blocks in AuthRepositoryImpl methods

--- src/main/kotlin/com/mvnh/database/repositories/impl/AuthRepositoryImpl.kt
+++ src/main/kotlin/com/mvnh/database/repositories/impl/AuthRepositoryImpl.kt
@@ -14,27 +14,32 @@ import org.jetbrains.exposed.sql.update
 import org.mindrot.jbcrypt.BCrypt
 
 class AuthRepositoryImpl : AuthRepository {
-    override suspend fun register(credentials: AccountCredentials): Boolean = suspendTransaction {
-        val existingAccount = AccountDAO.find {
-            AccountsTable.username eq credentials.username
-        }.firstOrNull()
-
-        require(existingAccount == null) { "Username already exists" }
+    override suspend fun register(credentials: AccountCredentials): Boolean {
         require(credentials.username.length in 4..32) { "Username must be between 4 and 32 characters" }
         require(credentials.password.length in 8..64) { "Password must be between 8 and 64 characters" }
 
-        AccountDAO.new {
-            username = credentials.username
-            password = BCrypt.hashpw(credentials.password, BCrypt.gensalt())
-        }
+        return suspendTransaction {
+            val existingAccount = AccountDAO.find {
+                AccountsTable.username eq credentials.username
+            }.firstOrNull()
 
-        true
+            require(existingAccount == null) { "Username already exists" }
+
+            AccountDAO.new {
+                username = credentials.username
+                password = BCrypt.hashpw(credentials.password, BCrypt.gensalt())
+            }
+
+            true
+        }
     }
 
-    override suspend fun login(credentials: AccountCredentials): AuthToken = suspendTransaction {
-        val account = AccountDAO.find {
-            AccountsTable.username eq credentials.username
-        }.firstOrNull()
+    override suspend fun login(credentials: AccountCredentials): AuthToken {
+        val account = suspendTransaction {
+            AccountDAO.find {
+                AccountsTable.username eq credentials.username
+            }.firstOrNull()
+        }
 
         require(account != null) { "Account not found" }
         require(BCrypt.checkpw(credentials.password, account.password)) { "Invalid password" }
@@ -42,22 +47,26 @@ class AuthRepositoryImpl : AuthRepository {
         val accessTokenToGrant = generateAccessToken(credentials.username)
         val refreshTokenToGrant = account.refreshToken ?: run {
             val newRefreshToken = generateRefreshToken(credentials.username)
-            AccountsTable.update({ AccountsTable.username eq credentials.username }) {
-                it[refreshToken] = newRefreshToken
+            suspendTransaction {
+                AccountsTable.update({ AccountsTable.username eq credentials.username }) {
+                    it[refreshToken] = newRefreshToken
+                }
             }
             newRefreshToken
         }
 
-        AuthToken(
+        return AuthToken(
             accessToken = accessTokenToGrant,
             refreshToken = refreshTokenToGrant
         )
     }
 
-    override suspend fun refresh(oldRefreshToken: String): AuthToken = suspendTransaction {
-        val account = AccountDAO.find {
-            AccountsTable.refreshToken eq oldRefreshToken
-        }.firstOrNull()
+    override suspend fun refresh(oldRefreshToken: String): AuthToken {
+        val account = suspendTransaction {
+            AccountDAO.find {
+                AccountsTable.refreshToken eq oldRefreshToken
+            }.firstOrNull()
+        }
         require(account != null) { "Invalid refresh token" }
 
         val accountRefreshToken = verifyToken(oldRefreshToken, JWTConfig.refreshAlgorithm)
@@ -70,11 +79,14 @@ class AuthRepositoryImpl : AuthRepository {
 
         val newAccessToken = generateAccessToken(account.username)
         val newRefreshToken = generateRefreshToken(account.username)
-        AccountsTable.update({ AccountsTable.username eq account.username }) {
-            it[refreshToken] = newRefreshToken
+
+        suspendTransaction {
+            AccountsTable.update({ AccountsTable.username eq account.username }) {
+                it[refreshToken] = newRefreshToken
+            }
         }
 
-        AuthToken(
+        return AuthToken(
             accessToken = newAccessToken,
             refreshToken = newRefreshToken
         )
